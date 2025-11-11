97a7ebfaa1bf4006a02bab85bc373b9f16edab51
Amir Mohseni <mramirmohseni@gmail.com>
2025-11-12 00:46:06 +0330

    feat(settings): add development environment config with PostgreSQL

---
+++ src/user_management_api/envs/development.py
@@ -0,0 +1,14 @@
+from .common import *
+from decouple import config
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
+    }
+}
