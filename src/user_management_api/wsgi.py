54ad555f91830c8970863638b6181a4d995635be
Amir Mohseni <mramirmohseni@gmail.com>
2025-11-12 00:47:17 +0330

    refactor(settings): update wsgi.py to use envs.development settings module

--- src/user_management_api/wsgi.py
+++ src/user_management_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user_management_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user_management_api.envs.development")
 
 application = get_wsgi_application()
